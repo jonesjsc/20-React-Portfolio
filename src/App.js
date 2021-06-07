0e113e4c06b191a13861aee2a070b2fb1a35c2e8
Jason Jones <jason.e.jones@gmail.com>
2021-06-06 23:51:04 -0400

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+// import React from "react";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className='App'>Hello from react</div>;
+  return <div>Hello from react</div>;
 }
 
 export default App;
