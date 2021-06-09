dfe57f09b22103b53cc06f81bb6f1642caf906ec
Jason Jones <jason.e.jones@gmail.com>
2021-06-08 21:37:26 -0400

    slight tweaks to starter code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 // import React from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div>Hello from react</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
